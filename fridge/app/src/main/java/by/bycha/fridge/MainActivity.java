ce923be825ea5a3f94ff8ec0ca880ab83cd4c497
Yauheni Bychkou <ybychkou@gomel.iba.by>
2016-04-12 19:10:30 +0300

    complite view of  launcher layout

--- fridge/app/src/main/java/by/bycha/fridge/MainActivity.java
+++ fridge/app/src/main/java/by/bycha/fridge/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initToolBar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+
     }
 
     private void initNavigationView() {
