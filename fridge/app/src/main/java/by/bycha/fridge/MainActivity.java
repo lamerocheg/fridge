6cd8a98d6d934d98a633d5c6424a7c0524be6b28
Yauheni Bychkou <ybychkou@gomel.iba.by>
2016-04-12 19:07:18 +0300

    complite view of  launcher layout

--- fridge/app/src/main/java/by/bycha/fridge/MainActivity.java
+++ fridge/app/src/main/java/by/bycha/fridge/MainActivity.java
@@ -1,5 +1,8 @@
 package by.bycha.fridge;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,16 +10,35 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
-
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initToolBar();
+        initNavigationView();
     }
+
+
     private void initToolBar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.app_name);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this , drawerLayout , toolbar , R.string.toggle_open , R.string.toggle_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                drawerLayout.closeDrawers();
+                return false;
+            }
+        });
 
     }
 }
